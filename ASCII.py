4756deacccc642656a65f545c74928947fe7550d
Priyanka Agrawal <65527151+Priyanka-tech798@users.noreply.github.com>
2020-05-27 23:52:21 +0530

    Create ASCII.py

---
+++ ASCII.py
@@ -0,0 +1,7 @@
+#ord() converts the given string of character in to ASCII value
+c = 'p'
+print("The ASCII value of",c,"is ",ord(c))
+
+#chr() takes on integer values and returns a character wrt that unicode
+i = 65
+print("Character of ASCII value ",i,"is ",chr(i))
