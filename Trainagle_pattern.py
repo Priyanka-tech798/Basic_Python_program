18ef8efa4090d665782bbf47d7c541ef91c2a8ac
Priyanka Agrawal <65527151+Priyanka-tech798@users.noreply.github.com>
2020-06-20 23:47:09 +0530

    Create Trainagle_pattern.py

---
+++ Trainagle_pattern.py
@@ -0,0 +1,24 @@
+n=int(input("Enter a value: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    
+    
+Enter a value: 5
+*
+* *
+* * *
+* * * *
+* * * * *
+    
+n=int(input("Enter a value: "))
+for i in range(n):
+    print("*"*(n-i))
+    
+Enter a value: 5
+*****
+****
+***
+**
+*
