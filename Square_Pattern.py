0a1da676beb7def7c50a544434e79c2d09f7ab55
Priyanka Agrawal <65527151+Priyanka-tech798@users.noreply.github.com>
2020-06-20 23:32:48 +0530

    Update Square_Pattern.py

--- Square_Pattern.py
+++ Square_Pattern.py
@@ -1,21 +1,34 @@
-n=int(input("Enter a value: "))  #5
+n=int(input("Enter a value: "))
 for i in range(n):
     print("*",end=" ")
 
-#o/p->  * * * * *
+Enter a value: 5
+* * * * *
 
 
 
-n=int(input("Enter a value: ")) #5
+n=int(input("Enter a value: ")) 
 for i in range(n):
     print("*"*n)
 
-
-output->
-
+Enter a value: 5
 *****
 *****
 *****
 *****
 *****
 
+
+n=int(input("Enter a value: "))
+for i in range(n):
+    print((str(i+1)+" ")*n)
+
+Enter a value: 8
+1 1 1 1 1 1 1 1
+2 2 2 2 2 2 2 2
+3 3 3 3 3 3 3 3
+4 4 4 4 4 4 4 4
+5 5 5 5 5 5 5 5
+6 6 6 6 6 6 6 6
+7 7 7 7 7 7 7 7
+8 8 8 8 8 8 8 8
