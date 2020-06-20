b0cf6b8bfb076e92e5380b0983a4d456e0c07ebd
Priyanka Agrawal <65527151+Priyanka-tech798@users.noreply.github.com>
2020-06-20 23:28:06 +0530

    Create Square_Pattern.py

---
+++ Square_Pattern.py
@@ -0,0 +1,21 @@
+n=int(input("Enter a value: "))  #5
+for i in range(n):
+    print("*",end=" ")
+
+#o/p->  * * * * *
+
+
+
+n=int(input("Enter a value: ")) #5
+for i in range(n):
+    print("*"*n)
+
+
+output->
+
+*****
+*****
+*****
+*****
+*****
+
